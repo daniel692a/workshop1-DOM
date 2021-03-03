f64d13dabf83f2145984a21ea592af2c6ade3e4c
DanielArmR <daniel62armas@gmail.com>
2021-03-02 20:54:36 -0600

    Add fetch api to consume avocado api

--- src/index.js
+++ src/index.js
@@ -3,4 +3,25 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-console.log('Happy hacking :)')
+const url = 'https://platzi-avo.vercel.app/api/avo';
+
+const fetchAvocados = async() => {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        const avocados = [];
+        data.data.forEach(e => {
+            const img = document.createElement('img');
+            img.src = `https://platzi-avo.vercel.app/${e.image}`;
+            img.title = `${e.name}`;
+            img.alt = `${e.name}`;
+            const h1 = document.createElement('h1');
+            h1.textContent = `${e.name}`;
+            avocados.push(img, h1);
+        });
+        document.body.append(...avocados);
+    } catch (error) {
+        console.error(error);
+    }
+}
+fetchAvocados();
