8e60e9f3409dd88ae33f5bf118c01a57b461eab0
DanielArmR <DanielArmR@users.noreply.github.com>
2021-03-04 20:43:37 +0000

    deploy: 63b8009a57399c51c6a21a4f0041f350b9c3741f

--- _dist_/index.js
+++ _dist_/index.js
@@ -2,24 +2,42 @@
  * This file is just a silly example to show everything working in the browser.
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
+const baseUrl = 'https://platzi-avo.vercel.app';
+const appNode = document.querySelector('#app');
+appNode.className = 'container-avocados';
 
-const url = 'https://platzi-avo.vercel.app/api/avo';
+const formatPrice = (price) =>{
+    /* API de internacionalización */
+    const newPrice = new window.Intl.NumberFormat('en', {
+        style: 'currency',
+        currency: 'USD'
+    }).format(price);
+    return newPrice;
+}
 
 const fetchAvocados = async() => {
     try {
-        const response = await fetch(url);
+        const response = await fetch(`${baseUrl}/api/avo`);
         const data = await response.json();
         const avocados = [];
         data.data.forEach(e => {
-            const img = document.createElement('img');
-            img.src = `https://platzi-avo.vercel.app/${e.image}`;
-            img.title = `${e.name}`;
-            img.alt = `${e.name}`;
-            const h1 = document.createElement('h1');
-            h1.textContent = `${e.name}`;
-            avocados.push(img, h1);
+            const imgAvocado = document.createElement('img');
+            imgAvocado.src = `${baseUrl}${e.image}`;
+            imgAvocado.title = e.name;
+            imgAvocado.alt = e.name;
+            imgAvocado.className = 'img-avocado';
+            const nameAvocado = document.createElement('h2');
+            nameAvocado.textContent = e.name;
+            nameAvocado.className = 'text-xl text-center';
+            const price = document.createElement('p');
+            price.textContent = `Price: ${formatPrice(e.price)}`;
+            price.className = 'text-center';
+            const container = document.createElement('div');
+            container.append(imgAvocado, nameAvocado, price);
+            container.className = 'rounded-lg bg-green-200 bg-opacity-50';
+            avocados.push(container);
         });
-        document.body.append(...avocados);
+        appNode.append(...avocados);
     } catch (error) {
         console.error(error);
     }
