c3dd9e60e47948dbe9ec44106a53b79b48cd610e
daniel692a <daniel692a@users.noreply.github.com>
2021-03-05 21:00:26 +0000

    deploy: 8ecad87ef68079d895b5e631737c11c0ff818f8e

--- _dist_/index.js
+++ _dist_/index.js
@@ -6,11 +6,11 @@ const baseUrl = 'https://platzi-avo.vercel.app';
 const appNode = document.querySelector('#app');
 appNode.className = 'container-avocados';
 /* Deelegación de eventos */
-appNode.addEventListener('click', (event) => {
+/* appNode.addEventListener('click', (event) => {
     if(event.target.nodeName === 'H2') {
         alert(event.target.innerText);
     }
-})
+}) */
 
 const formatPrice = (price) =>{
     /* API de internacionalización */
@@ -20,7 +20,14 @@ const formatPrice = (price) =>{
     }).format(price);
     return newPrice;
 }
-
+const not = document.querySelector('#notify');
+const description = document.querySelector('#description');
+const titleAvocado = document.querySelector('#name');
+const exit = document.querySelector('#close');
+exit.addEventListener('click', () =>{
+    not.classList.remove('show');
+    not.classList.add('hide');
+})
 const fetchAvocados = async() => {
     try {
         const response = await fetch(`${baseUrl}/api/avo`);
@@ -34,13 +41,19 @@ const fetchAvocados = async() => {
             imgAvocado.className = 'img-avocado';
             const nameAvocado = document.createElement('h2');
             nameAvocado.textContent = avocado.name;
-            nameAvocado.className = 'text-xl text-center text-green-200 font-semibold';
+            nameAvocado.className = 'text-xl text-center text-green-200 font-semibold cursor-pointer';
             const price = document.createElement('p');
             price.textContent = `Price: ${formatPrice(avocado.price)}`;
             price.className = 'text-center text-green-50';
             const container = document.createElement('div');
             container.append(imgAvocado, nameAvocado, price);
             container.className = 'rounded-lg';
+            nameAvocado.addEventListener('click', () => {
+                not.classList.remove('hide');
+                not.classList.add('show');
+                titleAvocado.textContent = avocado.name;
+                description.textContent = avocado.attributes.description;
+            });
             avocados.push(container);
         });
         appNode.append(...avocados);
