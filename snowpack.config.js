2a39aa7c29b677304cd9bae229bc322374f1018e
DanielArmR <daniel62armas@gmail.com>
2021-03-04 21:34:10 -0600

    Edit snowpak config

--- snowpack.config.js
+++ snowpack.config.js
@@ -5,6 +5,6 @@ module.exports = {
     src: '/_dist_',
   },
   buildOptions: {
-    baseUrl: 'https://DanielArmR.github.io/workshop1-DOM',
+    baseUrl: 'https://daniel692a.github.io/workshop1-DOM',
   },
 }
